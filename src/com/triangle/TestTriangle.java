2fb9e8551526a9fb043c8c34cab5d7dabc1cb4db
nishadmehendale <nishadmehendale@gmail.com>
2018-08-03 00:38:53 +0530

    Add files via upload

---
+++ src/com/triangle/TestTriangle.java
@@ -0,0 +1,37 @@
+package com.triangle;
+
+import java.util.Scanner;
+
+//Class TestTriangle that tests class Triangle and displays triangle type
+public class TestTriangle {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// Accepting input
+		System.out.println("Enter number of triangles");
+		int num = sc.nextInt();
+		int sides[] = new int[3];
+		Triangle[] triangle = new Triangle[num];
+		for (int i = 0; i < num; i++) {
+			triangle[i] = new Triangle();
+			System.out.println("Enter the sides");
+			sides[0] = sc.nextInt();
+			sides[1] = sc.nextInt();
+			sides[2] = sc.nextInt();
+			triangle[i].setSide(sides);
+			System.out.println(triangle[i]);
+			if (triangle[i].isRight(triangle[i])) {
+				System.out.println("Triangle is right angled");
+			}
+			if (triangle[i].isScalene(triangle[i])) {
+				System.out.println("Triangle is scalene");
+			} else if (triangle[i].isEquilateral(triangle[i])) {
+				System.out.println("Triangle is equilateral");
+			} else if (triangle[i].isIsosceles(triangle[i])) {
+				System.out.println("Triangle is isosceles");
+			}
+		}
+		sc.close();
+	}
+}
---
+++ src/com/triangle/Triangle.java
@@ -0,0 +1,58 @@
+package com.triangle;
+
+import java.util.Arrays;
+
+//Class Triangle that checks the type of the triangle
+public class Triangle {
+	private int[] side = new int[3];
+
+	public int[] getSide() {
+		return side;
+	}
+
+	public void setSide(int[] side) {
+		this.side = side;
+	}
+
+	// Method to check if a triangle is right angled triangle
+	public boolean isRight(Triangle T) {
+		int sideSquare1 = T.side[0] * T.side[0];
+		int sideSquare2 = T.side[1] * T.side[1];
+		int sideSquare3 = T.side[2] * T.side[2];
+
+		if ((sideSquare1 == sideSquare2 + sideSquare3) || (sideSquare2 == sideSquare1 + sideSquare3)
+				|| (sideSquare3 == sideSquare1 + sideSquare2))
+			return true;
+		else
+			return false;
+	}
+
+	// Method to check if a triangle is scalene
+	public boolean isScalene(Triangle T) {
+		if (T.side[0] != T.side[1] && T.side[1] != T.side[2] && T.side[2] != T.side[0])
+			return true;
+		else
+			return false;
+	}
+
+	// Method to check if a triangle is iscosceles
+	public boolean isIsosceles(Triangle T) {
+		if (T.side[0] == T.side[1] || T.side[1] == T.side[2] || T.side[2] == T.side[0])
+			return true;
+		else
+			return false;
+	}
+
+	// method to check if a triangle is equilateral
+	public boolean isEquilateral(Triangle T) {
+		if (T.side[0] == T.side[1] && T.side[1] == T.side[2] && T.side[2] == T.side[0])
+			return true;
+		else
+			return false;
+	}
+
+	@Override
+	public String toString() {
+		return "Triangle [side=" + Arrays.toString(side) + "]";
+	}
+}
